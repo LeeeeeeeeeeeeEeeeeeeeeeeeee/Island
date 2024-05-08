5125b955f671acb304292ef2bbc01c6f5115263c
akrehd2 <76907967+akrehd2@users.noreply.github.com>
2024-05-08 12:14:46 +0900

    add

---
+++ Assets/scr/CellCtrl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellCtrl : MonoBehaviour
+{
+    public bool isOn = false;
+
+    public int Id;
+    public string cellName;
+    public Texture cellImage;
+}
